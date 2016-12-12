608c5f2d421b152e24c67ad7365d9fd1b743026b
ifdog <ifdog@live.cn>
2016-12-12 19:47:27 +0800

    Initial commit

---
+++ app/src/main/java/com/molymade/apps/molybeads/BeadsActivity.java
@@ -0,0 +1,13 @@
+package com.molymade.apps.molybeads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeadsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beads);
+    }
+}
